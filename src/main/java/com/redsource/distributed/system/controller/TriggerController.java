c6a180142720f1adc83e0781996eaedd2dc4aaeb
paulvincentcabunilas <paul.cabunilas@doctena.com>
2022-08-04 22:17:44 +0200

    [Fix] debug

--- src/main/java/com/redsource/distributed/system/controller/TriggerController.java
+++ src/main/java/com/redsource/distributed/system/controller/TriggerController.java
@@ -2,7 +2,6 @@ package com.redsource.distributed.system.controller;
 
 import com.redsource.distributed.system.service.PublisherService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
